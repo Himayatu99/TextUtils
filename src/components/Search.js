f3bc4ebf20190be428ad6b843ab6456d38085f3d
khan <himayatu99@gmail.com>
2022-01-10 13:12:18 +0500

    Add : Search for image

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,7 +9,7 @@ const Search = () => {
     return (
         <>
 
-            <h1 className="text-center font-bold font-mono text-lg mb-5">Search only Image</h1>
+            <h1 className="text-center font-bold font-mono text-lg mb-3">Search only Image</h1>
             <div className="flex items-center justify-center ">
                 <div className="flex border-2 border-gray-200 rounded">
                     <input onChange={handleCh} value={img} type="text" className="px-4 py-2 w-80" placeholder="Search Only Image" />
